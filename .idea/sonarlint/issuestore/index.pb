4a20a00f7c1abec66383b80f254fca8ee31815ab
James Taylor <j.taylor@kainos.com>
2019-08-21 17:27:31 +0100

    first commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,16 @@
+
+F
+assets/js/game_over.js,9/9/9924fb483937c2558170c2b396a4e3ba0b744d47
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
+:
+
+index.html,f/6/f6013a00b362253c64368d6eebc50ea2131754e2
+9
+	server.js,d/c/dc60ea9ee1ff6246bf342463277d5b914db2883d
+A
+assets/js/main.js,2/7/27535fc9a68fbb5975536329c4632c8cca0d3e2a
+A
+assets/js/menu.js,3/d/3d12b4ae5caf414366be875e0de6211d3a9df15e
+A
+assets/js/game.js,b/2/b215c7a1160886c3ddbf7f1acbf83cf2a36cb261
---
+++ assets/js/menu.js
@@ -0,0 +1,24 @@
+var Menu = {
+
+  preload : function() {
+    // Load all the needed resources for the menu.
+    game.load.image('menu', './assets/images/menu.png');
+  },
+
+  create: function () {
+
+    // Add menu screen.
+    // It will act as a button to start the game.
+    this.add.button(0, 0, 'menu', this.startGame, this);
+
+  },
+
+  startGame: function () {
+
+    game.state.add('Game',Game);
+    // Change the state to the actual game.
+    this.state.start('Game');
+
+  }
+
+};
